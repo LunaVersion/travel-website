9ee29fd4aceb3a90720120d8e4e094f3fd01cd4d
LunaVersion <146586199+LunaVersion@users.noreply.github.com>
2024-08-11 18:56:41 +0700

    Update PostController.php

--- backend-api/app/Http/Controllers/PostController.php
+++ backend-api/app/Http/Controllers/PostController.php
@@ -47,7 +47,14 @@ class PostController extends Controller
                 'draft' => 'required|json',
             ]);
     
-            $post = $this->service->create($request->all());
+            $post = Post::create([
+                'category_id' => $request->category_id,
+                'title' => $request->title,
+                'slug' => Str::slug($request->title),
+                'content' => $request->content,
+                'status' => $request->status,
+                'image' => $request->image,
+            ]);
     
             return response()->json($post, 201);
         } catch (\Exception $exception) {
@@ -71,7 +78,26 @@ class PostController extends Controller
                 'draft' => 'required|json',
             ]);
     
-            $post = $this->service->create($request->all());
+            $draftData = [
+                'category_id' => $request->category_id,
+                'title' => $request->title,
+                'slug' => Str::slug($request->title),
+                'content' => $request->content,
+                'status' => 0, 
+                'image' => $request->image,
+            ];
+
+            if ($request->has('id')) {
+                // Cập nhật nháp từ bài viết chính hiện có
+                $post = Post::findOrFail($request->id);
+                $post->draft = json_encode($draftData);
+                $post->save();
+            } else {
+                // Tạo nháp mới
+                $post = Post::create([
+                    'draft' => json_encode($draftData),
+                ]);
+            }
     
             return response()->json($post, 201);
         } catch (\Exception $exception) {
@@ -82,42 +108,41 @@ class PostController extends Controller
     }
 
     // Cập nhật bài viết + lưu thành bản nháp
-    public function updatePostToDraft(Request $request, $id)
-    {
-        try {
-            $request->validate([
-                'category_id' => 'required|exists:categories,id',
-                'title' => 'required|string|max:255',
-                'slug' => 'required|string|max:255|unique:posts,slug,' . $id,
-                'content' => 'nullable|string',
-                'status' => 'required|tinyInteger',
-                'image' => 'nullable|string',
-                'draft' => 'required|json',
-            ]);
+    // public function updatePostToDraft(Request $request, $id)
+    // {
+    //     try {
+    //         $request->validate([
+    //             'category_id' => 'required|exists:categories,id',
+    //             'title' => 'required|string|max:255',
+    //             'slug' => 'required|string|max:255|unique:posts,slug,' . $id,
+    //             'content' => 'nullable|string',
+    //             'status' => 'required|tinyInteger',
+    //             'image' => 'nullable|string',
+    //             'draft' => 'required|json',
+    //         ]);
     
-            $post = Post::findOrFail($id);
+    //         $post = Post::findOrFail($id);
     
-            $draft = [
-                'category_id' => $request->category_id,
-                'title' => $request->title,
-                'slug' => $request->slug,
-                'content' => $request->content,
-                'status' => 0, 
-                'image' => $request->image, 
-            ];
+    //         $draft = [
+    //             'category_id' => $request->category_id,
+    //             'title' => $request->title,
+    //             'slug' => $request->slug,
+    //             'content' => $request->content,
+    //             'status' => 0, 
+    //             'image' => $request->image, 
+    //         ];
     
-            $post->draft = json_encode($draft);
+    //         $post->draft = json_encode($draft);
     
-            $post->save();
+    //         $post->save();
     
-            return response()->json($post, 200);
-        } catch (\Exception $exception) {
-            \Log::error('Login error: ' . $exception->getMessage());
-            return response()->json(['error' => 'Có lỗi xảy ra'], 500);
-        } 
+    //         return response()->json($post, 200);
+    //     } catch (\Exception $exception) {
+    //         \Log::error('Login error: ' . $exception->getMessage());
+    //         return response()->json(['error' => 'Có lỗi xảy ra'], 500);
+    //     } 
         
-    }
-
+    // }
 
     public function update(Request $request, $id)
     {
@@ -136,30 +161,17 @@ class PostController extends Controller
             if (!$post) {
                 return response()->json(['error' => 'Bài viết không tìm thấy'], 404);
             }
-    
-            // nếu có nháp
-            if ($request->has('draft') && $post->draft) {
-                // sửa trên nháp
-                $post->update([
-                    'category_id' => $request->category_id,
-                    'title' => $request->title,
-                    'slug' => Str::slug($request->title),
-                    'content' => $request->content,
-                    'status' => 0, 
-                    'image' => $request->image,
-                ]);
-            } else {
-                $post->update([
-                    'category_id' => $request->category_id,
-                    'title' => $request->title,
-                    'slug' => Str::slug($request->title),
-                    'content' => $request->content,
-                    'status' => $request->status,
-                    'image' => $request->image,
-                    'draft' => null, 
-                ]);
-            }
-    
+            
+            $post->update([
+                'category_id' => $request->category_id,
+                'title' => $request->title,
+                'slug' => Str::slug($request->title),
+                'content' => $request->content,
+                'status' => $request->status,
+                'image' => $request->image,
+                'draft' => null, 
+            ]);
+        
             return response()->json($post);
         } catch (\Exception $exception) {
             \Log::error('Login error: ' . $exception->getMessage());
@@ -167,6 +179,42 @@ class PostController extends Controller
         }  
         
     }
+
+    public function updateDraft(Request $request, $id)
+    {
+        try {
+            $request->validate([
+                'category_id' => 'required|exists:categories,id',
+                'title' => 'required|string|max:255',
+                'slug' => 'required|string|max:255|unique:posts,slug,' . $id,
+                'content' => 'nullable|string',
+                'status' => 'required|integer',
+                'image' => 'nullable|string',
+            ]);
+
+            $post = Post::find($id);
+            if (!$post) {
+                return response()->json(['error' => 'Bài viết không tìm thấy'], 404);
+            }
+
+            $post->update([
+                'draft' => json_encode([
+                    'category_id' => $request->category_id,
+                    'title' => $request->title,
+                    'slug' => Str::slug($request->title),
+                    'content' => $request->content,
+                    'status' => 0, 
+                    'image' => $request->image,
+                ]),
+            ]);
+
+            return response()->json($post);
+        } catch (\Exception $exception) {
+            \Log::error('Draft update error: ' . $exception->getMessage());
+            return response()->json(['error' => 'Có lỗi xảy ra'], 500);
+        }
+    }
+
 
     public function destroy($id)
     {
@@ -187,32 +235,3 @@ class PostController extends Controller
         
     }
 }
-
-    // public function updateDraft(Request $request, $id)
-    // {
-    //     $request->validate([
-    //         'category_id' => 'required|exists:categories,id',
-    //         'title' => 'required|string|max:255',
-    //         'slug' => 'required|string|max:255|unique:posts,slug,' . $id,
-    //         'draft' => 'required|json',
-    //         'content' => 'required|string',
-    //         'status' => 'required|integer',
-    //         'image' => 'nullable|string',
-    //     ]);
-
-    //     $post = Post::find($id);
-    //     if (!$post) {
-    //         return response()->json(['error' => 'Bài viết không tìm thấy'], 404);
-    //     }
-
-    //     $post->update([
-    //         'category_id' => 'required|exists:categories,id',
-    //         'title' => $request->title,
-    //         'slug' => Str::slug($request->title),
-    //         'content' => $request->content,
-    //         'status' => $request->status,
-    //         'image' => $request->image,
-    //         'draft' => $request->draft,
-    //     ]);
-    //     return response()->json($post);
-    // }
